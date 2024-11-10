7ed805b2c3062b869caf85bc438014880ad3bfe4
Shriya Rasale <srasale1031@gmail.com>
2024-11-09 19:29:29 -0500

    calendar updates

--- frontend/src/app/calendar/page.tsx
+++ frontend/src/app/calendar/page.tsx
@@ -5,18 +5,21 @@ import MyCalendar from '@/myCalendar/page';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[#082A69] p-6">
-      <Head>
-        <title>Next.js Calendar App</title>
-        <meta name="description" content="A simple calendar app using React Big Calendar and Next.js" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <Navbar />
+      <div className="min-h-screen bg-[#082A69] p-6"> {/* Set the background color to 082A69 */}
+        <main className="max-w-4xl mx-auto space-y-8 bg-white rounded-lg shadow-lg p-6">
+          <Head>
+            <title>Next.js Calendar App</title>
+            <meta name="description" content="A simple calendar app using React Big Calendar and Next.js" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
 
-      <main className="max-w-4xl mx-auto space-y-8 bg-white rounded-lg shadow-lg p-6">
-        <h1 className="text-[#2b6fed]'text-3xl font-semibold text-center text-[#082A69]">Calendar</h1>
-       
-        <MyCalendar />
-      </main>
-    </div>
+          <h1 className="text-[#2b6fed] text-3xl font-semibold text-center text-[#082A69]">Calendar</h1>
+
+          <MyCalendar />
+        </main>
+      </div>
+    </>
   );
 }
